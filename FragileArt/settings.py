a1a89a578e6521aab01465c03be96991dc6b3951
WisamTa <wisamtaqaly@gmail.com>
2022-05-27 21:07:27 +0000

    try to deploy

--- FragileArt/settings.py
+++ FragileArt/settings.py
@@ -198,6 +198,11 @@ USE_TZ = True
 
 if 'USE_AWS' in os.environ:
 
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
 
     AWS_STORAGE_BUCKET_NAME = 'fragileart1'
     AWS_S3_REGION_NAME = 'EU (Stockholm) eu-north-1'
