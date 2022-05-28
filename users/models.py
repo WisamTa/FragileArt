ef2bfcc15ca5ddc3f4960075b8703c3a901be935
WisamTa <wisamtaqaly@gmail.com>
2022-05-28 15:52:40 +0000

    trying to fix import error

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
+from six import python_2_unicode_compatible
+
 
 class UserProfile(models.Model):
     """
